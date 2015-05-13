69f20d06ccc1ac45147461eebd578d3c52233ca5
utr <utr@users.noreply.github.com>
2015-05-13 15:29:11 -0500

    Update fastbaseball.py
    Added check for length of list

--- fastbaseball.py
+++ fastbaseball.py
@@ -11,30 +11,59 @@ tree = html.fromstring(page.text)
 
 scores = tree.xpath('//*[@id="inning"]/td/text()')
 
-firstTop = scores[0]
-firstBottom = scores[1]
-secondTop = scores[2]
-secondBottom = scores[3]
-thirdTop = scores[4]
-thirdBottom = scores[5]
-fourthTop = scores[6]
-fourthBottom = scores[7]
-fifthTop = scores[8]
-fifthBottom = scores[9]
-sixthTop = scores[10]
-sixthBottom = scores[11]
-seventhTop = scores[12]
-seventhBottom = scores[13]
-eighthTop = scores[14]
-eighthBottom = scores[15]
-ninthTop = scores[16]
-
-#if not scores[17]:
-#   print "Empty Element"
-
-#print scores
-
-#ninthBottom = scores[17] // create an if statement that shows or doesn't show this
+if len(scores) >= 1:
+    firstTop = scores[0]
+    
+if len(scores) >= 2:
+    firstBottom = scores[1]
+    
+if len(scores) >= 3:
+    secondTop = scores[2]
+    
+if len(scores) >= 4:
+    secondBottom = scores[3]
+    
+if len(scores) >= 5:
+    thirdTop = scores[4]
+    
+if len(scores) >= 6:
+    thirdBottom = scores[5]
+    
+if len(scores) >= 7:
+    fourthTop = scores[6]
+    
+if len(scores) >= 8:
+    fourthBottom = scores[7]
+    
+if len(scores) >= 9:
+    fifthTop = scores[8]
+    
+if len(scores) >= 10:
+    fifthBottom = scores[9]
+    
+if len(scores) >= 11:
+    sixthTop = scores[10]
+    
+if len(scores) >= 12:
+    sixthBottom = scores[11]
+    
+if len(scores) >= 13:
+    seventhTop = scores[12]
+    
+if len(scores) >= 14:
+    seventhBottom = scores[13]
+    
+if len(scores) >= 15:
+    eighthTop = scores[14]
+    
+if len(scores) >= 16:
+    eighthBottom = scores[15]
+    
+if len(scores) >= 17:
+    ninthTop = scores[16]
+    
+if len(scores) >= 18:
+    ninthBottom = scores[17]
 
 skip = ""
 
